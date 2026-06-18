5ed9dd83698579edd49e98bd1802fd10a6b40fd2
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-06-17 19:18:22 -0600

    Main

--- src/main/java/pg07algoritmos/controller/MainController.java
+++ src/main/java/pg07algoritmos/controller/MainController.java
@@ -1,8 +1,14 @@
 package pg07algoritmos.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import pg07algoritmos.model.Tree.*;
 
+import java.util.List;
+import java.util.Random;
 
 
 public class MainController {
@@ -11,6 +17,473 @@ public class MainController {
     private ComboBox cbGrafos;
     @FXML
     private ComboBox cbTree;
+    @FXML
+    private Canvas canvasTree;
+    @FXML
+    private Button btnPlayBTree;
+    @FXML
+    private Canvas canvasBTree;
+    @FXML
+    private Button addAristas;
+    @FXML
+    private TabPane mainTabs;
+    @FXML
+    private Button generateTree;
+    @FXML
+    private Button btnPlayBst;
+    @FXML
+    private TextArea txtOutput;
+
+    private BTree<Integer> bTree;
+    private BST<Integer> bst;
+    private AVL<Integer> avl;
+
+    private GraphicsContext gc;
+    private Random random;
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    public void initialize(){
+
+        gc = canvasTree.getGraphicsContext2D();
+
+        random = new Random();
+
+        bTree = new BTree<>();
+        bst = new BST<>();
+        avl = new AVL<>();
+
+        cbTree.getItems().addAll(
+                "Árbol Binario",
+                "BST",
+                "AVL"
+        );
+
+        cbTree.getSelectionModel().selectFirst();
+
+        generateTree.setOnAction(e->generateRandomTree());
+
+
+
+    }
+
+    private void generateRandomTree(){
+
+        String tipo = cbTree.getValue().toString();
+
+        switch (tipo){
+
+            case "Árbol Binario":
+
+                generateBinaryTree();
+                break;
+
+            case "BST":
+
+                generateBST();
+                break;
+
+            case "AVL":
+
+                generateAVL();
+                break;
+        }
+
+    }
+
+    private void generateBinaryTree(){
+
+        bTree.clear();
+
+        for(int i=0;i<15;i++){
+
+            bTree.addBFS(random.nextInt(100)+1);
+
+        }
+
+        drawBinaryTree();
+
+        showBinaryMethods(bTree);
+
+    }
+
+    private void generateBST(){
+
+        bst.clear();
+
+        for(int i=0;i<15;i++){
+
+            bst.add(random.nextInt(100)+1);
+
+        }
+
+        drawBST();
+
+        showBSTMethods();
+
+    }
+
+    private void generateAVL(){
+
+        avl.clear();
+
+        for(int i=0;i<15;i++){
+
+            avl.add(random.nextInt(100)+1);
+
+        }
+
+        drawAVL();
+
+        showAVLMethods();
+
+    }
+
+
+    private void drawBinaryTree() {
+
+        gc.clearRect(0, 0, canvasTree.getWidth(), canvasTree.getHeight());
+
+        drawBackground();
+
+        TreePainter.drawSimpleNode(
+                gc,
+                bTree.root,
+                canvasTree.getWidth() / 2,
+                60,
+                160,
+                true
+        );
+    }
+
+    private void drawBST() {
+
+        gc.clearRect(0, 0, canvasTree.getWidth(), canvasTree.getHeight());
+
+        drawBackground();
+
+        TreePainter.drawSimpleNode(
+                gc,
+                bst.root,
+                canvasTree.getWidth() / 2,
+                60,
+                160,
+                true
+        );
+    }
+
+    private void drawAVL() {
+
+        gc.clearRect(0, 0, canvasTree.getWidth(), canvasTree.getHeight());
+
+        drawBackground();
+
+        TreePainter.drawTreeNode(
+                gc,
+                avl.root,
+                canvasTree.getWidth() / 2,
+                60,
+                160,
+                avl,
+                true
+        );
+    }
+
+    private void drawBackground() {
+
+        gc.setFill(Color.web("#071A2F"));
+
+        gc.fillRect(
+                0,
+                0,
+                canvasTree.getWidth(),
+                canvasTree.getHeight()
+        );
+
+        gc.setStroke(Color.web("#1B3554"));
+
+        for (int x = 0; x < canvasTree.getWidth(); x += 40) {
+
+            gc.strokeLine(
+                    x,
+                    0,
+                    x,
+                    canvasTree.getHeight()
+            );
+        }
+
+        for (int y = 0; y < canvasTree.getHeight(); y += 40) {
+
+            gc.strokeLine(
+                    0,
+                    y,
+                    canvasTree.getWidth(),
+                    y
+            );
+        }
+    }
+
+    private void showBinaryMethods(BTree<Integer> tree) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("===== Binary Tree =====\n\n");
+
+        sb.append(tree.toString()).append("\n");
+
+        sb.append("1. printNodesWithChildren()\n");
+        sb.append(tree.printNodesWithChildren()).append("\n");
+
+        sb.append("2. printNodes1Child()\n");
+        sb.append(tree.printNodes1Child()).append("\n");
+
+        sb.append("3. printNodes2Child()\n");
+        sb.append(tree.printNodes2Child()).append("\n");
+
+        sb.append("4. printLeaves()\n");
+        sb.append(tree.printLeaves()).append("\n");
+
+        sb.append("5. grandFather()\n");
+
+        printGrandFather(tree.root, tree, sb);
+
+        sb.append("\n");
+
+        sb.append("6. father()\n");
+
+        printFather(tree.root, tree, sb);
+
+        sb.append("\n");
+
+        sb.append("7. brother()\n");
+
+        printBrother(tree.root, tree, sb);
+
+        sb.append("\n");
+
+        sb.append("8. cousins()\n");
+
+        printCousins(tree.root, tree, sb);
+
+        sb.append("\n");
+
+        sb.append("9. printSubtree()\n");
+
+        printSubtrees(tree.root, tree, sb);
+
+        sb.append("\n");
+
+        sb.append("10. totalLeaves()\n");
+        sb.append(tree.totalLeaves());
+
+        txtOutput.setText(sb.toString());
+
+    }
+    private void showBSTMethods() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("== BST ==\n\n");
+
+        sb.append(bst.toString()).append("\n");
+
+        sb.append("1. printNodesWithChildren()\n");
+        sb.append(bst.printNodesWithChildren()).append("\n");
+
+        sb.append("2. printNodes1Child()\n");
+        sb.append(bst.printNodes1Child()).append("\n");
+
+        sb.append("3. printNodes2Child()\n");
+        sb.append(bst.printNodes2Child()).append("\n");
+
+        sb.append("4. printLeaves()\n");
+        sb.append(bst.printLeaves()).append("\n");
+
+        sb.append("5. grandFather()\n");
+
+        printGrandFather(bst.root, bst, sb);
+
+        sb.append("\n");
+
+        sb.append("6. father()\n");
+
+        printFather(bst.root, bst, sb);
+
+        sb.append("\n");
+
+        sb.append("7. brother()\n");
+
+        printBrother(bst.root, bst, sb);
+
+        sb.append("\n");
+
+        sb.append("8. cousins()\n");
+
+        printCousins(bst.root, bst, sb);
+
+        sb.append("\n");
+
+        sb.append("9. printSubtree()\n");
+
+        printSubtrees(bst.root, bst, sb);
+
+        sb.append("\n");
+
+        sb.append("10. totalLeaves()\n");
+        sb.append(bst.totalLeaves());
+
+        txtOutput.setText(sb.toString());
+
+    }
+
+    private void showAVLMethods() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("== AVL ==\n\n");
+
+        sb.append(avl.toString()).append("\n");
+
+        sb.append("1. printNodesWithChildren()\n");
+        sb.append(avl.printNodesWithChildren()).append("\n");
+
+        sb.append("2. printNodes1Child()\n");
+        sb.append(avl.printNodes1Child()).append("\n");
+
+        sb.append("3. printNodes2Child()\n");
+        sb.append(avl.printNodes2Child()).append("\n");
+
+        sb.append("4. printLeaves()\n");
+        sb.append(avl.printLeaves()).append("\n");
+
+        sb.append("5. grandFather()\n");
+
+        printGrandFather(avl.root, avl, sb);
+
+        sb.append("\n");
+
+        sb.append("6. father()\n");
+
+        printFather(avl.root, avl, sb);
+
+        sb.append("\n");
+
+        sb.append("7. brother()\n");
+
+        printBrother(avl.root, avl, sb);
+
+        sb.append("\n");
+
+        sb.append("8. cousins()\n");
+
+        printCousins(avl.root, avl, sb);
+
+        sb.append("\n");
+
+        sb.append("9. printSubtree()\n");
+
+        printSubtrees(avl.root, avl, sb);
+
+        sb.append("\n");
+
+        sb.append("10. totalLeaves()\n");
+        sb.append(avl.totalLeaves());
+
+        txtOutput.setText(sb.toString());
+
+    }
+
+    private void printGrandFather(BTreeNode<Integer> node,
+                                  BTree<Integer> tree,
+                                  StringBuilder sb){
+
+        if(node == null)
+            return;
+
+        sb.append("grandFather(")
+                .append(node.data)
+                .append("): ")
+                .append(tree.grandFather(node.data))
+                .append("\n");
+
+        printGrandFather(node.left, tree, sb);
+        printGrandFather(node.right, tree, sb);
+
+    }
+
+    private void printFather(BTreeNode<Integer> node,
+                             BTree<Integer> tree,
+                             StringBuilder sb){
+
+        if(node == null)
+            return;
+
+        sb.append("father(")
+                .append(node.data)
+                .append("): ")
+                .append(tree.father(node.data))
+                .append("\n");
+
+        printFather(node.left, tree, sb);
+        printFather(node.right, tree, sb);
+
+    }
+
+    private void printBrother(BTreeNode<Integer> node,
+                              BTree<Integer> tree,
+                              StringBuilder sb){
+
+        if(node == null)
+            return;
+
+        sb.append("brother(")
+                .append(node.data)
+                .append("): ")
+                .append(tree.brother(node.data))
+                .append("\n");
+
+        printBrother(node.left, tree, sb);
+        printBrother(node.right, tree, sb);
+
+    }
+
+    private void printCousins(BTreeNode<Integer> node,
+                              BTree<Integer> tree,
+                              StringBuilder sb){
+
+        if(node == null)
+            return;
+
+        sb.append("cousins(")
+                .append(node.data)
+                .append("): ")
+                .append(tree.cousins(node.data))
+                .append("\n");
+
+        printCousins(node.left, tree, sb);
+        printCousins(node.right, tree, sb);
+
+    }
+
+    private void printSubtrees(BTreeNode<Integer> node,
+                               BTree<Integer> tree,
+                               StringBuilder sb){
+
+        if(node == null)
+            return;
+
+        sb.append("Subtree(")
+                .append(node.data)
+                .append("): ")
+                .append(tree.printSubtree(node.data))
+                .append("\n");
+
+        printSubtrees(node.left, tree, sb);
+        printSubtrees(node.right, tree, sb);
+
+    }
+
 
 
 
--- src/main/java/pg07algoritmos/model/Tree/BST.java
+++ src/main/java/pg07algoritmos/model/Tree/BST.java
@@ -123,4 +123,281 @@ public class BST<T extends Comparable<T>> extends BTree<T> {
         }
         return result;
     }
+
+    /**
+     * Practica para examen: árboles binarios
+     **/
+
+    /**
+     * 1.Devuelve un String con todos los nodos del árbol que
+     * tienen hijos(uno o dos hijos), junto con sus hijos
+     * Es parecido al preorder
+     **/
+
+    public String printNodesWithChildren(){
+
+        if(isEmpty()) return "Binary Tree is empty";
+
+        StringBuilder result = new StringBuilder();
+        printNodesWithChildren(root, result);//Inicio revisando la raiz
+
+        return result.toString();
+    }
+
+    //Usar recursividad
+    private void printNodesWithChildren(BTreeNode<T> node,  StringBuilder result){
+        if(node == null)//Caso base
+            return;
+
+        if(node.left != null || node.right != null){
+
+            result.append(node.data).append(" -> ");
+
+            if(node.left != null) result.append(node.left.data).append(" ");
+            if(node.right != null) result.append(node.right.data).append(" ");
+            result.append("\n");
+        }
+
+        printNodesWithChildren(node.left, result);
+        printNodesWithChildren(node.right, result);
+
+
+    }
+
+    /**
+     *2.Devuelve un string con todos los nodos del árbol
+     *que tienen exactamente un hijo, junto con su hijo
+     * Puede ser parecido al primero
+     **/
+
+    public String printNodes1Child(){
+
+        if(isEmpty()) return "Binary Tree is empty";
+
+        StringBuilder result = new StringBuilder();
+        printNodes1Child(root, result);//Inicio revisando la raiz
+
+        return result.toString();
+    }
+
+    //Metodo recursivo
+    private void printNodes1Child(BTreeNode<T> node,  StringBuilder result){
+        if(node == null)
+            return;
+
+        if (node.left != null && node.right == null) {
+            result.append("Dad: ").append(node.data).append("  Child: ").append(node.left.data).append("  \n");
+        }
+        else if (node.right != null && node.left == null) {
+            result.append("Dad: ").append(node.data).append("  Child: ").append(node.right.data).append("  \n");
+        }
+        printNodes1Child(node.left, result);
+        printNodes1Child(node.right, result);
+
+    }
+
+    /**
+     *3.Devuelve un string con todos los nodos del árbol
+     * que tienen dos hijos, junto con su hijo
+     * Es parecido a este solo hay que cambiar la condición
+     **/
+    public String printNodes2Child(){
+
+        if(isEmpty()) return "Binary Tree is empty";
+
+        StringBuilder result = new StringBuilder();
+        printNodes2Child(root, result);
+
+        return result.toString();
+    }
+
+    //Metodo recursivo
+    private void printNodes2Child(BTreeNode<T> node,  StringBuilder result){
+        if (node == null) {
+            return;
+        }
+
+        if(node.left  != null && node.right!= null)
+            result.append("Dad: ").append(node.data).append("  Child: ")
+                    .append(node.left.data).append("  ").append(node.right.data).append("  ");
+        result.append("\n");
+
+        printNodes2Child(node.left, result);
+        printNodes2Child(node.right, result);
+    }
+
+    /**
+     *4.Devuelve un string con todos los nodos del árbol
+     * que no tenan hijos
+     * La candición es que que izquierda y dereca sean  nulos
+     **/
+    public String printLeaves(){
+
+        if(isEmpty()) return "Binary Tree is empty";
+
+        StringBuilder result = new StringBuilder();
+        printLeaves(root, result);
+
+        return result.toString();
+    }
+
+    //Metodo recursivo
+    private void printLeaves(BTreeNode<T> node,  StringBuilder result){
+        if(node == null)
+            return;
+
+        if(node.left == null && node.right == null)
+            result.append("Leaves: ").append(node.data).append("\n");
+
+
+        printLeaves(node.left, result);
+        printLeaves(node.right, result);
+    }
+
+    /**
+     *5.Devuelve el abuelo de elemento dado.Si no tiene abuelo
+     * debera de indicar: no tiene abuelo
+     **/
+    public Object grandFather(T element) {
+
+        if(isEmpty()) return "Binary Tree is empty";
+        BTreeNode<T> parent = findFather(root, element);
+
+        if(parent == null || parent == root) return "No tiene abuelo";
+
+        BTreeNode<T> grandFather = findFather(root, parent.data);
+
+        if(grandFather != null) return grandFather.data;
+        else return "No tiene abuelo";
+    }
+
+    /**
+     *6.Devuelve el padre del elemento dado. Si no tiene padre debera
+     * de indicar. no tiene padre
+     **/
+    public Object father(T element) {
+
+        if(isEmpty()) return "Binary Tree is empty";
+        if (equals(root.data, element)) return "El nodo es la raiz, no tiene padre";
+
+        BTreeNode<T> node = findFather(root,element);
+        if(node == null) return "El nodo no tiene padre";
+
+        return node.data;
+    }
+
+    public BTreeNode<T> findFather(BTreeNode<T> node,T element){
+
+        if(node == null) return null;
+
+        if(node.left != null && equals(node.left.data,element) ||
+                (node.right != null && equals(node.right.data,element))){
+            return node;
+        }
+
+        BTreeNode<T> father = findFather(node.left,element);
+        if(father != null) return father;
+
+        return findFather(node.right,element);
+
+    }
+    /**
+     *7.Devuelve el hermano del elemento dado. Si no tiene hermano debera
+     * de indicar que no tiene hermano
+     **/
+    public Object brother(T element) {
+        if(isEmpty()) return "Binary Tree is empty";
+        if(equals(root.data, element)) return "No tiene hermano";
+
+        BTreeNode<T> node = findFather(root,element);
+
+        if(node == null) return "No tiene hermano";
+
+        if(node.left != null && equals(node.left.data,element)){
+            return (node.right != null ? node.right.data : "No tiene hermano");
+        }
+
+        if(node.right != null && equals(node.right.data,element)){
+            return (node.left != null ? node.left.data : "No tiene hermano");
+        }
+
+        return "No tiene hermano";
+    }
+    /**
+     *8.Devuelve el primo de un elemento dado. Si no tienen primo debera
+     * de indicar que no tiene primo
+     **/
+    public Object cousins(T element) {
+
+        if(isEmpty()) return "Binary Tree is empty";
+
+        if(equals(root.data, element)) return "No tiene primos";
+
+        BTreeNode<T> parent = findFather(root, element);
+
+        if(parent == null) return "No tiene primos";
+
+        BTreeNode<T> grandFather = findFather(root, parent.data);
+
+        if(grandFather == null) return "No tiene primos";
+
+        BTreeNode<T> uncle = null;
+
+        if(grandFather.left == parent)
+            uncle = grandFather.right;
+        else if(grandFather.right == parent)
+            uncle = grandFather.left;
+
+        if(uncle == null) return "No tiene primos";
+
+        StringBuilder sb = new StringBuilder();
+
+        addChild(uncle, sb);
+
+        return sb.length() == 0 ? "No tiene primos" : sb.toString();
+    }
+
+    private void addChild(BTreeNode<T> node, StringBuilder sb){
+        if (node.left != null) sb.append(node.left.data).append(" ");
+        if (node.right != null) sb.append(node.right.data).append(" ");
+    }
+    /**
+     *9.Devuelve un String con todos los elementos
+     * que conforman el subárbol a partir del nodo del elemento dado.
+     **/
+    public String printSubtree(T element) {
+        BTreeNode<T> subRoot = buscarNodo(root, element);
+
+        if (subRoot == null) return "Elemento no encontrado";
+        return preOrder(subRoot);
+    }
+    private BTreeNode<T> buscarNodo(BTreeNode<T> node, T element) {
+        if (node == null) return null;
+        if (equals(node.data, element)) return node;
+
+        BTreeNode<T> left = buscarNodo(node.left, element);
+        if (left != null) return left;
+        return buscarNodo(node.right, element);
+    }
+    /**
+     *10.Devuelve un entero indicando el
+     * número de hojas que tienen el árbol
+     * La candición es que que izquierda y dereca sean  nulos y ahi sumar
+     **/
+    public int totalLeaves(){
+
+        if(isEmpty()) return 0;
+
+        return totalLeaves(root);
+    }
+
+    private int totalLeaves(BTreeNode<T> node){
+        if(node == null) return 0;
+
+        if(node.left == null && node.right == null)
+            return 1;
+
+        return totalLeaves(node.left) + totalLeaves(node.right);
+    }
+
 }
